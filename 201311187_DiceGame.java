373e28385cf8d91c2be702ab8462bc8105785961
201311187 강태훈 <rkdxl1234@gmail.com>
2016-10-18 20:00:45 +0900

    Add files via upload

---
+++ 201311187_DiceGame.java
@@ -0,0 +1,18 @@
+﻿class DiceGame{
+   Player p1;
+   Player p2;
+   public void startGame() {
+      p1 = new Player("A");
+      p2 = new Player("B");
+      p1.play();
+      p2.play();
+      if(p1.getNum() > p2.getNum()){
+         System.out.print(p1.getName() + "won");
+      }
+      else if(p1.getNum() == p2.getNum()){
+         System.out.println("draw");
+      }
+      else
+         System.out.println(p2.getName() + "won");
+   }
+}
