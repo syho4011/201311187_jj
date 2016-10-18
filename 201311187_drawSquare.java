c172d71a3ae0044749fc7a7e64ba1219f3237b20
201311187 강태훈 <rkdxl1234@gmail.com>
2016-10-18 21:00:41 +0900

    Add files via upload

---
+++ 201311187_drawSquare.java
@@ -0,0 +1,6 @@
+﻿void drawSquare(int size){
+   for(int i=0; i<4; i++){
+   t1.right(90);
+   t1.forward(size);
+   }
+}
---
+++ 201311187_drawStar.java
@@ -0,0 +1,8 @@
+﻿void drawStar(int size){
+   t1.right(90);
+   t1.forward(size);
+   for(int i=0; i<4; i++){
+      t1.right(144);
+      t1.forward(size);
+   }
+}
