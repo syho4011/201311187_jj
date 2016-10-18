78e1b4c4972da3f87aed6d7873c59526812d0daa
201311187 강태훈 <rkdxl1234@gmail.com>
2016-10-18 20:00:27 +0900

    Add files via upload

---
+++ 201311187_Dice.java
@@ -0,0 +1,10 @@
+﻿class Dice{
+   private int number = 0;
+   public void roll(){
+      number = (int)(Math.random()*6);
+      System.out.println("Turn on" + number);
+   }
+   public int getNum(){
+      return this.number;
+   }
+}
