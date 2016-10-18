cc35ed1f6a4b682dbd206dc8fc745e63d61ce045
201311187 강태훈 <rkdxl1234@gmail.com>
2016-10-18 20:00:36 +0900

    Add files via upload

---
+++ 201311187_Player.java
@@ -0,0 +1,18 @@
+﻿class Player{
+   private String name=null;
+   int num=0;
+   public Player(String _name) {
+      name=_name;
+   }
+   public void play(){
+      dice d=new dice();
+      d.roll();
+      num=d.getNum();
+   }
+   public String getName(){
+      return this.name;
+   }
+   public int getNum(){
+      return this.num;
+   }
+}
