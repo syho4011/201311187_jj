601c4d424f335ceb9a0f72f646fb2141cf47256f
201311187 강태훈 <rkdxl1234@gmail.com>
2016-10-25 04:17:32 +0900

    Add files via upload

---
+++ 201311187_Calendar.java
@@ -0,0 +1,11 @@
+﻿import java.util.Calendar;
+
+Calendar cal = Calendar.getInstance();
+int year = cal.get(Calendar.YEAR);
+int month = cal.get(Calendar.MONTH);      
+int day = cal.get(Calendar.DAY_OF_MONTH);
+int hour = cal.get(Calendar.HOUR_OF_DAY);
+int minute = cal.get(Calendar.MINUTE);
+int second = cal.get(Calendar.SECOND);
+
+System.out.printf("Now is %4d/%02d/%02d %02d:%02d:%02d\n", year, month+1, day, hour, minute, second);
