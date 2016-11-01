aa1c1b26eb76aa14966621b6f3ea351b7ed0cdf7
201311187 강태훈 <rkdxl1234@gmail.com>
2016-11-01 23:18:33 +0900

    Add files via upload

---
+++ makeSwirlSquare.java
@@ -0,0 +1,17 @@
+﻿import ch.aplu.turtle.*;
+
+Turtle t1 = new Turtle()
+
+void makeSwirlSquare(int size, int bigger, int turns, double degree){
+   for(int i=0; i<turns; i++){
+      if(i%2 !=0){
+         size+=bigger;
+      }
+      t1.forward(size);
+      t1.right(degree);
+   }
+}
+
+
+
+makeSwirlSquare(10,15,20,90.0)
