4cf3b1873d493358de851c71e37955207c48057b
201311187 강태훈 <rkdxl1234@gmail.com>
2016-11-02 00:27:11 +0900

    Add files via upload

---
+++ SwirlSquare and FC and AF/201311187_Temperature.java
@@ -0,0 +1,18 @@
+﻿class Temp {
+   double toC(double f) {
+      double c = (f - 32) * (5.0 / 9.0);
+      return c;
+   }
+   double toF(double c){
+      double f =  c * (9.0 / 5.0) + 32; 
+      return f;
+   }
+   
+
+   public static void main(String[] args) {
+      Temp t = new Temp();
+      System.out.println(t.toC(75)); 
+      System.out.println(t.toF(90));
+   }
+}
+
